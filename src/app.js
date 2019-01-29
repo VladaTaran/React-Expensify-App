bc0f2e105b1e50d21e13e89d2ced885b7d3f319a
Vlada <n.vladyslava@gmail.com>
2019-01-29 22:34:01 +0200

    added functional for Add, Edit and Remove expense

--- src/app.js
+++ src/app.js
@@ -15,10 +15,6 @@ store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
 store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
 store.dispatch(addExpense({description: 'Rent', amount: 109000}));
 
-setTimeout(() => {
-    store.dispatch(setTextFilter('water'));
-}, 3000)
-
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
