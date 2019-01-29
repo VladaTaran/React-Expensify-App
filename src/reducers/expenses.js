9973d1eba007d5fb75f31cabe31b8e838a292ffa
Vlada <n.vladyslava@gmail.com>
2019-01-29 20:21:24 +0200

    Added React Redux. Organizing redux

---
+++ src/reducers/expenses.js
@@ -0,0 +1,28 @@
+  //Expenses Reducer
+const expensesReducerDafaultState = [];
+
+const expensesReducer = (state = expensesReducerDafaultState, action) => {
+  switch (action.type) {
+      case 'ADD_EXPENSE':
+          return [
+              ...state,
+              action.expense
+          ];
+      case 'REMOVE_EXPENSE':
+          return state.filter(expense => expense.id !== action.id);
+      case 'EDIT_EXPENSE':
+          return state.map((expense) => {
+            if (expense.id === action.id) {
+              return {
+                ...expense,
+                ...action.updates
+              };
+            } else {
+              return expense
+            };
+          });
+      default: return state;
+  }
+};
+
+export default expensesReducer;
